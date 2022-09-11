d6c712ad697118a3147f1cfee3962086fbce434d
unknown <whdgnl7479@naver.com>
2022-09-12 02:56:38 +0900

    styled refactoring

---
+++ components/post-list/post-list.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  max-width: 1330px;
+  margin: 0 auto;
+`;
+
+export const CardUl = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  gap: 0.3rem;
+
+  justify-content: center;
+`;
