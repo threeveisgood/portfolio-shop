688f1d112426084d5f8d4e745fa2994ba0dce60b
threeveisgood <whdgnl7479@naver.com>
2023-02-17 20:52:40 +0900

    error 리턴

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,12 @@ module.exports = {
     },
   },
   extends: [
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     "plugin:import/recommended",
     "plugin:react-hooks/recommended",
-    "next/core-web-vitals",
   ],
   rules: {
     "react/react-in-jsx-scope": 0,
@@ -26,8 +26,6 @@ module.exports = {
     "import/no-unresolved": 0,
     "import/named": 2,
     "import/order": 1,
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off",
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
